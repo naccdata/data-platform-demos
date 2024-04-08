fa8f1cec54546a477067a9b440c9dd6272ccec67
Ben Keller <bjkeller@vt.edu>
2024-04-08 00:07:26 +0000

    Add script to set virtual environment

---
+++ set-venv.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ROOTS=$(pants roots)
+python3 -c "print('PYTHONPATH=\"./' + ':./'.join('''${ROOTS}'''.split('\n')) + ':\$PYTHONPATH\"')" > .env
+
+if [ ! -e python-default.lock ]; then
+    pants generate-lockfiles
+else
+    echo "Skipping generation of lock file"
+fi
+
+pants export --py-resolve-format=symlinked_immutable_virtualenv --resolve=python-default
+
+ln -snf dist/export/python/virtualenvs/python-default /workspaces/form-upload-demo/venv
